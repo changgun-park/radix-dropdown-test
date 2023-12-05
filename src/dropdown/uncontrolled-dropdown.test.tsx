d0052034596b8b03c453afbc0f4112f1a14653f3
Jackson <cg.park@3billion.io>
2023-12-05 13:44:46 +0900

    chores: minor changes..

--- src/dropdown/uncontrolled-dropdown.test.tsx
+++ src/dropdown/uncontrolled-dropdown.test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { UncontrolledDropdown } from "./uncontrolled-dropdown";
 
-it("renders without crashing", async () => {
+it("clicking on 'trigger' opens up menu", async () => {
   const user = userEvent.setup();
 
   render(<UncontrolledDropdown />);
 
   await user.click(screen.getByText("Trigger"));
-  expect(screen.getByText("Item 1")).toBeInTheDocument();
+  expect(screen.getByRole("menu")).toBeInTheDocument();
 });
