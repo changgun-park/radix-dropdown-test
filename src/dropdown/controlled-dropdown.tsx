2401f9b8a5ae54ce478a3ad678fe8d16c873e2c5
Jackson <cg.park@3billion.io>
2023-12-05 13:13:24 +0900

    feat: controlled, uncontrolled dropdown 생성

---
+++ src/dropdown/controlled-dropdown.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
+/**
+ *
+ */
+export function ControlledDropdown() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <DropdownMenu.Root open={open}>
+      <DropdownMenu.Trigger onClick={() => setOpen(true)}>
+        Trigger
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item>Item 1</DropdownMenu.Item>
+          <DropdownMenu.Item>Item 2</DropdownMenu.Item>
+          <DropdownMenu.Item>Item 3</DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
---
+++ src/dropdown/uncontrolled-dropdown.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+
+/**
+ *
+ */
+export function UncontrolledDropdown() {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>Trigger</DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item>Item 1</DropdownMenu.Item>
+          <DropdownMenu.Item>Item 2</DropdownMenu.Item>
+          <DropdownMenu.Item>Item 3</DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
